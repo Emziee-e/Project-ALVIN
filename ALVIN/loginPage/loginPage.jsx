cb7f39c673dad6e62ac12c8ded1dd7756aa10b0b
John Manuel <2301565@ub.edu.ph>
2026-04-05 16:38:06 +0800

    added color to login page

--- ALVIN/loginPage/loginPage.jsx
+++ ALVIN/loginPage/loginPage.jsx
@@ -1,17 +1,28 @@
-const loginHeader = <h1 class="loginHeader">Login</h1>
 import './loginPage.css';
 
 function LoginPage() {
     return (
-        <div>
-            {loginHeader}
-            <form>
-                <label htmlFor="email">Email Address: </label>
-                <input type="email" id="email" name="email" /> <br />
-                <br /><label htmlFor="password">Password: </label>
-                <input type="password" id="password" name="password" /> <br />
-                <br /> <button type="submit">Login</button>
-            </form>
+        <div className="loginContainer">
+            <div className="logoSection">
+                <div className="logoPlaceholder">
+                    <span>Your Logo Here</span>
+                </div>
+            </div>
+            <div className="formSection">
+                <h1 className="loginHeader">Login</h1>
+                <form className="loginForm">
+                    <div className="formGroup">
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" id="email" name="email" placeholder="Enter your email" />
+                    </div>
+                    <div className="formGroup">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter your password" />
+                    </div>
+                    <button type="submit" className="loginButton">Sign In</button>
+                </form>
+                <p className="signupText">Don't have an account? <a href="#signup">Sign up</a></p>
+            </div>
         </div>
     )
 }   
